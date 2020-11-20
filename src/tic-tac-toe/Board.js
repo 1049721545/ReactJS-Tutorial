590719f66891f751e25b3d1a1e4612762cbdd4b9
Brav0 <50433581+1049721545@users.noreply.github.com>
2020-11-20 19:46:10 +1300

    Added board component for later use AB#32

---
+++ src/tic-tac-toe/Board.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Grid } from "semantic-ui-react";
+import _ from "lodash";
+import Square from "./Square";
+
+function Board(props) {
+  const rows = _.times(props.size, (row) => (
+    <Grid.Row columns={props.size}>{cols(row)}</Grid.Row>
+  ));
+
+  function cols(row) {
+    const items = [];
+
+    for (let col = 0; col < props.size; col++) {
+      let id = row * props.size + col;
+
+      items.push(
+        <Grid.Column width={1}>
+          <Square
+            value={props.board && props.board[id]}
+            onClick={() => props.onClick(id)}
+          />
+        </Grid.Column>
+      );
+    }
+
+    return items;
+  }
+
+  return (
+    <Container style={{ margin: 10 }}>
+      <Grid centered>{rows}</Grid>
+    </Container>
+  );
+}
+
+export default Board;
